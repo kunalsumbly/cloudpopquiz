664f594d4b5f024e938f1f386d081eaa0857fa15
Kunal Sumbly <kunalsumbly@Kunals-MBP-2.modem>
2023-04-08 23:31:41 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Flashcards() {
+    const [cards, setCards] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [flipped, setFlipped] = useState([]);
+
+    useEffect(() => {
+        fetch('/flashcards.json')
+            .then((response) => response.json())
+            .then((data) => {
+                setCards(data);
+                setFlipped(Array(data.length).fill(false));
+            });
+    }, []);
+
+    function handleFlip(index) {
+        setFlipped((prevState) => {
+            const flippedCards = [...prevState];
+            flippedCards[index] = !flippedCards[index];
+            return flippedCards;
+        });
+    }
+
+    function handleNextCard() {
+        setCurrentIndex((currentIndex + 1) % cards.length);
+    }
+
+    function handlePrevCard() {
+        setCurrentIndex((currentIndex + cards.length - 1) % cards.length);
+    }
+
+    return (
+        <div className="flashcards-container">
+            {cards.length > 0 ? (
+                <div className="flashcard">
+                    <div
+                        className={`flashcard-inner ${
+                            flipped[currentIndex] ? 'flipped' : ''
+                        }`}
+                        onClick={() => handleFlip(currentIndex)}
+                    >
+                        <div className="flashcard-front">
+                            <div className="flashcard-text">{cards[currentIndex].question}</div>
+                        </div>
+                        <div className="flashcard-back">
+                            <div className="flashcard-text">{cards[currentIndex].answer}</div>
+                        </div>
+                    </div>
+                    <div className="buttons">
+                        <button onClick={handlePrevCard}>Prev</button>
+                        <button onClick={handleNextCard}>Next</button>
+                    </div>
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default Flashcards;
